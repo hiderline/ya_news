5dca8e07c272c3ee631637462bf97ea6b5eb2c75
Dmitrii Popov <hiderline@yandex.ru>
2024-07-10 17:19:15 +0700

    test_content implemented

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,10 +1,21 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
 
-from news.models import News
+from news.forms import CommentForm
+from news.models import Comment, News
+
+from datetime import datetime, timedelta
+
+User = get_user_model()
 
 
-class TestContent(TestCase):
+class TestHomePage(TestCase):
+
+    # Вынесем ссылку на домашнюю страницу в атрибуты класса.
+    HOME_URL = reverse('news:home')
 
     # фикстуры: нужно создать более десятка новостей, тогда можно будет понять,
     # ограничивается ли их количество при выводе на главную.
@@ -14,12 +25,118 @@ class TestContent(TestCase):
     # больше, чем должно отобразиться на странице.
     # Первое же решение, которое приходит на ум — создать цикл
     # и на каждой итерации цикла создавать объекты.
-    @classmethod
-    def setUpTestData(cls):
-        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
-            News.objects.create(title=f'Новость {index}', text='Просто текст.')
+
+    # @classmethod
+    # def setUpTestData(cls):
+    #     for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
+    #         News.objects.create(title=f'Новость {index}', text='Просто текст.')
+
     # На каждой итерации будет отправляться запрос к БД.
     # Это как минимум неэффективно
     # Для одновременного создания нескольких объектов применяют метод
     # bulk_create()
-    
+    # Можно совместить это дело с list comprehension (списковое включение)
+    # прямо внутри News.objects.bulk_create(...)
+
+    @classmethod
+    def setUpTestData(cls):
+        # Вычисляем текущую дату
+        today = datetime.today()
+        News.objects.bulk_create(
+            News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                # Для каждой новости уменьшаем дату на index дней от today,
+                # где index - счётчик цикла.
+                date=today - timedelta(days=index)
+            )
+            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+        )
+
+    def test_news_count(self):
+        # Загружаем главную страницу.
+        response = self.client.get(self.HOME_URL)
+        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
+        # Получаем список объектов из словаря контекста.
+        object_list = response.context['object_list']
+        # Определяем количество записей в списке.
+        news_count = object_list.count()
+        # Проверяем, что на странице именно 10 новостей.
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
+
+    # Тестируем сортировку новостей
+        # Модифицируем фикстуру создания новостей таким образом,
+        # чтобы у каждой новости была собственная уникальная дата.
+    # Затем в тесте можно получить даты новостей и убедиться, что
+    # новости отсортированы в нужном порядке:
+        # получим список дат из новостей,
+        # создадим второй такой же список и отсортируем его,
+        # сравним списки: если исходный список равен отсортированному —
+        # значит, новости отсортированы правильно.
+    def test_news_order(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        # Получаем даты новостей в том порядке, как они выведены на странице.
+        all_dates = [news.date for news in object_list]
+        # Сортируем полученный список по убыванию.
+        sorted_dates = sorted(all_dates, reverse=True)
+        # Проверяем, что исходный список был отсортирован правильно.
+        self.assertEqual(all_dates, sorted_dates)
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Тестовая новость', text='Просто текст.'
+        )
+        # Сохраняем в переменную адрес страницы с новостью:
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        # Запоминаем текущее время:
+        now = timezone.now()
+        # Создаём комментарии в цикле.
+        for index in range(10):
+            # Создаём объект и записываем его в переменную.
+            comment = Comment.objects.create(
+                news=cls.news, author=cls.author, text=f'Tекст {index}',
+            )
+            # Сразу после создания меняем время создания комментария.
+            comment.created = now + timedelta(days=index)
+            # И сохраняем эти изменения.
+            comment.save()
+
+    def test_comments_order(self):
+        response = self.client.get(self.detail_url)
+        # Проверяем, что объект новости находится в словаре контекста
+        # под ожидаемым именем - названием модели.
+        self.assertIn('news', response.context)
+        # Получаем объект новости.
+        news = response.context['news']
+        # Получаем все комментарии к новости.
+        all_comments = news.comment_set.all()
+        # Собираем временные метки всех новостей.
+        all_timestamps = [comment.created for comment in all_comments]
+        # Сортируем временные метки, менять порядок сортировки не надо.
+        sorted_timestamps = sorted(all_timestamps)
+        # Проверяем, что временные метки отсортированы правильно.
+        self.assertEqual(all_timestamps, sorted_timestamps)
+
+    # Тест наличия формы в словаре контекста
+    # Будет 2 теста:
+        # ПЕРВЫЙ тест проверить, что при запросе анонимного пользователя
+        # форма не передаётся в словаре контекста
+        # ВТОРОЙ тест првоерит, что при запросе авторизованного пользователя
+        # форма передаётся в словаре контекста
+    def test_anonymous_client_has_no_form(self):
+        response = self.client.get(self.detail_url)
+        self.assertNotIn('form', response.context)
+
+    def test_authorized_client_has_form(self):
+        # Авторизуем клиент при помощи ранее созданного пользователя.
+        self.client.force_login(self.author)
+        response = self.client.get(self.detail_url)
+        self.assertIn('form', response.context)
+        # Проверим, что объект формы соответствует нужному классу формы.
+        self.assertIsInstance(response.context['form'], CommentForm)
